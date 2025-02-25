d29b644a660b99afe21639932597f5fcc51d65d5
dannielsLlumy <danielllumiquinga20102004@gmail.com>
2025-02-25 15:01:33 -0500

    Add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 export const routes: Routes = [
-    {
-        path: 'home',
-        component: HomeComponent
-    }
+    { path: 'home', component: HomeComponent },
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'formulario', component: FormularioComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: Pagina404Component },
 ];
--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './nosotros.component.css'
 })
 export class NosotrosComponent {
-
+  
 }
