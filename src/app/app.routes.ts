5840246b8754d0b3cfcf4329543beaf9c9d7d38d
DanielMP1995 <danielsantiagomeraparedes@gmail.com>
2025-02-26 10:36:22 -0500

    Valeria

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { Pagina404Component } from './pagina404/pagina404.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'nosotros', component: NosotrosComponent },
     { path: 'formulario', component: FormularioComponent },
+    {path:'contactos', component: ContactosComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: Pagina404Component },
 ];
