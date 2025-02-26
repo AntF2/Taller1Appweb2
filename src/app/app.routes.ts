f246a728e5f46203b2f81b41b45dacc916b499ac
AntF2 <lomasbueno5000@gmail.com>
2025-02-26 17:47:04 -0500

    Actualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NosotrosComponent } from './nosotros/nosotros.component';
-import { Pagina404Component } from './pagina404/pagina404.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { ContactosComponent } from './contactos/contactos.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { Pagina404Component } from './pages/pagina404/pagina404.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
 import { MascotasComponent } from './categoria/mascotas/mascotas.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'nosotros', component: NosotrosComponent },
     { path: 'formulario', component: FormularioComponent },
+    { path: 'productos', component: ProductosComponent },
     {path:'contactos', component: ContactosComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: Pagina404Component },
