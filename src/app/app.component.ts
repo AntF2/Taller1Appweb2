49c2a76d3f03d4c02dc5ad46c9aad95c3927e382
ValeriaXZ1 <lesvale568@gmail.com>
2025-03-02 18:09:34 -0500

    ESTILOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NavComponent],
+  imports: [RouterOutlet, RouterModule, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
